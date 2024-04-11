613ccbe95407be1af839b3140145224ae6ffb5ba
Kristina M <155168607+Kristina-Mar@users.noreply.github.com>
2024-04-11 19:49:02 +0200

    Trying out a pull request
    Edited the Console.Line texts.

--- Lesson1_homework/Program.cs
+++ Lesson1_homework/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("New course is here!");
+            Console.WriteLine("Yay!");
         }
     }
 }
