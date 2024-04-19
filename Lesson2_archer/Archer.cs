d9a210cf988f3bb283b8fbc3acf8bd6b4d0e10ef
Kristina M <kris.martiskova@gmail.com>
2024-04-19 20:16:37 +0200

    Exercise for methods and cycles
    Console app with an Archer class that lets an archer shoot arrows and a cycle that shoots all the arrows until the archer runs out.

---
+++ Lesson2_archer/Archer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_archer
+{
+    internal class Archer
+    {
+        public int NumberOfArrows = 10;
+        public void ShootArrow()
+        {
+            if (NumberOfArrows > 0)
+            {
+                NumberOfArrows--;
+                Console.WriteLine("Bullseye everytime, baby!");
+                if (NumberOfArrows == 0)
+                {
+                    Console.WriteLine("This was my last shot.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I'm out of arrows!");
+            }
+        }
+    }
+}
---
+++ Lesson2_archer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson2_archer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Archer legolas = new Archer();
+            while (legolas.NumberOfArrows > 0) // Fires all the arrows.
+            {
+                legolas.ShootArrow();
+            }
+            Console.ReadLine();
+            legolas.ShootArrow(); // The archer doesn't have any more arrows, the message will let the user know.
+            Console.ReadLine();
+        }
+    }
+}
