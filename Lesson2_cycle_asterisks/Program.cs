10ae56b16dd74d3f26075870e5c2f713a3a3eb5e
Kristina M <kris.martiskova@gmail.com>
2024-04-19 19:52:12 +0200

    Cycle that writes asterisks in the console

---
+++ Lesson2_cycle_asterisks/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson2_cycle_asterisks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many asterisks should I write?");
+            string input = Console.ReadLine();
+            int numberOfAsterisks;
+            bool isInputNumber = int.TryParse(input, out numberOfAsterisks);
+            while (!isInputNumber || numberOfAsterisks <= 0)
+            {
+                Console.WriteLine("I need an integer bigger than 0, try again:");
+                input = Console.ReadLine();
+                isInputNumber = int.TryParse(input, out numberOfAsterisks);
+            }
+            for (int i = 0; i < numberOfAsterisks; i++)
+            {
+                Console.Write("*");
+            }
+            Console.ReadLine();
+        }
+    }
+}
