6139e318d501d1c74e008917e537eed0b8f39d0c
Kristina M <kris.martiskova@gmail.com>
2024-04-29 19:22:39 +0200

    Deleted unnecessary null variable declarations

--- Lesson3_homework/Program.cs
+++ Lesson3_homework/Program.cs
@@ -43,37 +43,32 @@ it was the grayish white of old porridge.";
 
             // Do promenne 'delkaTextu' uloz celkovou delku uryvku z knizky.
 
-            int delkaTextu = 0;
             string ocistenyText = text.Replace("\r\n", " ");
             // Vyčistí prázdné řádky (mezery mezi odstavci), které se počítají nesprávně jako 4 znaky.
-            delkaTextu = ocistenyText.Length;
+            int delkaTextu = ocistenyText.Length;
             Console.WriteLine("Delka textu je spravna - ".PadRight(padding) + (delkaTextu == 1001));
 
             // Do promenne 'oddelovac' vloz text, ktery se sklada pouze z pomlcek a jeho delka je presne 20. Pouzij k tomu konstruktor typu string.
 
-            string oddelovac = null;
-            oddelovac = new string('-', 20);
+            string oddelovac = new string('-', 20);
             Console.WriteLine("Oddelovac ma 20 pomlcek - ".PadRight(padding) + (oddelovac == "--------------------"));
 
             // Pozmen nasledujici porovnani textu tak, aby se do konzole vypisovalo True, aniz bys menila hodnoty promennych
 
             string jmeno1 = "Katka";
             string jmeno2 = "katka";
-            bool jeStejne = false;
-            jeStejne = String.Equals(jmeno1, jmeno2, StringComparison.OrdinalIgnoreCase);
+            bool jeStejne = String.Equals(jmeno1, jmeno2, StringComparison.OrdinalIgnoreCase);
             Console.WriteLine("Obe promenne obsahuji stejne jmeno - ".PadRight(padding) + jeStejne);
 
             // Zjisti, jestli je v textu zminka o obtloustle "tete" Harryho. Jmenuje se Marge. Vysledek uloz do promenne 'piseSeOMarge';
 
-            bool piseSeOMarge = false;
-            piseSeOMarge = text.Contains("Marge"); // Vlastní jméno musí být velkým písmenem na začátku.
+            bool piseSeOMarge = text.Contains("Marge"); // Vlastní jméno musí být velkým písmenem na začátku.
             Console.WriteLine("V textu se zminuje Harryho 'teticka' - ".PadRight(padding) + (piseSeOMarge == true));
 
             // Zjisti, jestli je text spravne ukonceny interpunkci. Vysledek uloz do promenne 'jeSpravneUkoncen'.
 
-            bool jeSpravneUkoncen = false;
-            char posledniZnak = text[text.Length - 1];
-            jeSpravneUkoncen = posledniZnak == '.' || posledniZnak == '!' || posledniZnak == '?';
+            char posledniZnak = text[^1];
+            bool jeSpravneUkoncen = posledniZnak == '.' || posledniZnak == '!' || posledniZnak == '?';
             Console.WriteLine("Text je spravne ukoncen interpunkci - ".PadRight(padding) + (jeSpravneUkoncen == true));
 
             // Pomoci abecedniho porovnavani zjisti, ktery z nasledujicich textu je podle abecedy prvni a jeho hodnotu prirad do promenne 'prvni'.
@@ -81,7 +76,7 @@ it was the grayish white of old porridge.";
             string blabol1 = "abbc";
             string blabol2 = "acbc";
             string blabol3 = "abbb";
-            string prvni = null;
+            string prvni;
             if (String.Compare(blabol1, blabol2) <= 0)
             {
                 if (String.Compare(blabol1, blabol3) <= 0)
@@ -109,7 +104,6 @@ it was the grayish white of old porridge.";
 
             // Najdi prvni rozkazovaci vetu v textu a uloz ji do promenne 'veta' bez vykricniku a uvozovek.
 
-            string veta = null;
             int poziceVykricniku = text.IndexOf('!');
             string oseknutyZbytekTextu = text.Substring(0, poziceVykricniku); // Odstraní se text počínající vykřičníkem.
             int poziceZacatkuVety = 0;
@@ -132,7 +126,7 @@ it was the grayish white of old porridge.";
             {
                 poziceZacatkuVety++;
             }
-            veta = oseknutyZbytekTextu.Substring(poziceZacatkuVety);
+            string veta = oseknutyZbytekTextu.Substring(poziceZacatkuVety);
             Console.WriteLine("Prvni rozkazovaci veta je 'Hurry up, boy' - ".PadRight(padding) + (veta == "Hurry up, boy"));
 
             // Zjisti, kolikrat se v textu vyskytuje slovo "and" bez ohledu na velikosti prvniho pismenka a vysledek uloz do promenne 'pocetAnd'.
