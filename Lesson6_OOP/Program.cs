8ccad5dc5581c260f4922879802a58267e29f3fe
Kristina M <kris.martiskova@gmail.com>
2024-05-18 13:00:45 +0200

    Added clearer text to print into the console

--- Lesson6_OOP/Program.cs
+++ Lesson6_OOP/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Regular rectangle:");
+            Console.WriteLine("Regular rectangle 5.63 x 8:");
             RectangleCreation rectangle1 = new RectangleCreation(5.63, 8);
             rectangle1.PrintRectangleInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Increasing rectangle size:");
+            Console.WriteLine("Increasing rectangle size by 10 and 85.41:");
             rectangle1.ChangeSize(10, 85.41);
             rectangle1.PrintRectangleInfo();
             Console.WriteLine();
@@ -19,22 +19,22 @@
             rectangle1.PrintRectangleInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Rectangle with length of 0:");
+            Console.WriteLine("Rectangle 0 x 8:");
             RectangleCreation rectangle2 = new RectangleCreation(0, 8);
             rectangle2.PrintRectangleInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Regular square:");
+            Console.WriteLine("Regular square 5 x 5:");
             RectangleCreation square1 = new RectangleCreation(5);
             square1.PrintRectangleInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Increasing square size:");
+            Console.WriteLine("Increasing square side size by 20:");
             square1.ChangeSize(20);
             square1.PrintRectangleInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Square with negative side length:");
+            Console.WriteLine("Square -8.5 x -8.5:");
             RectangleCreation square2 = new RectangleCreation(-8.5);
             square2.PrintRectangleInfo();
             Console.WriteLine();
